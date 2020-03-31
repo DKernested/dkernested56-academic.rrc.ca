041f75e31be99de64f8c9d68b74deac775c61675
DkernestedRRC <62911994+DkernestedRRC@users.noreply.github.com>
2020-03-30 21:23:39 -0500

    updatedeletephp
    update delete posts. As it stands delete functions and update just spins forever. I see no reason for this.

---
+++ updatedelete.php
@@ -0,0 +1,61 @@
+<!--
+Dillon Kernested
+Web Dev
+Final Project Update Delete review page
+--->
+
+<?php
+
+	require "connect.php";
+	
+	if(isset($_POST['delete'])) 
+	{ 
+			//Checks for the ReviewId of the post and sanatizes it.
+			$ReviewId = filter_input(INPUT_POST, 'ReviewId', FILTER_SANITIZE_NUMBER_INT);
+
+			//creates the query to delete 
+			$deleteQuery = "DELETE FROM reviews WHERE ReviewId = :ReviewId LIMIT 1";
+
+			//return the statement 
+			$deleteStatement = $db->prepare($deleteQuery);
+
+			//bind the value to the delete statement
+			$deleteStatement->bindValue(':ReviewId', $ReviewId, PDO::PARAM_INT);
+
+			//Executes the statement 
+			$deleteStatement->execute();
+			header("Location: reviews.php");
+			exit;	
+	}
+
+	elseif(isset($_POST['update'])) 
+	{ 
+		//if the command is update, checks for values in Title/Content/ReviewId 
+		if(isset($_POST['Title']) && isset($_POST['Content']) && isset($_POST['ReviewId']))
+		{
+			//sanitizing the three values 
+			$Title = filter_input(INPUT_POST, 'Title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			$Content = filter_input(INPUT_POST, 'Content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			$ReviewId = filter_input(INPUT_POST, 'ReviewId', FILTER_SANITIZE_NUMBER_INT);
+		//	$BookId = filter_input(INPUT_POST, 'BookId', FILTER_SANITIZE_NUMBER_INT);
+
+			//update query statement
+			$updateQuery = "UPDATE reviews SET Title = :Title, Content = :Content WHERE ReviewId = :ReviewId";
+
+			//returns the update statement object
+			$updateStatement = $db->prepare($updateQuery);
+			//bind the value to Title, Content, and ReviewId
+			$updateStatement->bindValue(':Title', $Title);
+			$updateStatement->bindValue(':Content', $Content);
+			$updateStatement->bindValue(':ReviewId', $ReviewId, PDO::PARAM_INT);
+		//	$updateStatement->bindValue(':BookId', $ReviewId, PDO::PARAM_INT);
+			
+
+			//executes the update statement
+			$updateStatement->execute();
+
+			header("Location: reviews.php");
+			exit;
+		}
+	}
+?>
