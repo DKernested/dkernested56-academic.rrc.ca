1a24ffd97d736c2da69e6639f35fab2da3965b01
DkernestedRRC <62911994+DkernestedRRC@users.noreply.github.com>
2020-04-12 21:46:23 -0500

    login/logout

---
+++ login.php
@@ -0,0 +1,75 @@
+<?php
+
+require 'connect.php';
+session_start();
+
+if(isset($_POST['submit']))
+{
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+
+    if(isset($username) && isset($password)) 
+    {
+        $searchQuery = "SELECT * FROM users WHERE Username = '$username'";
+
+        $getUserStatement = $db->prepare($searchQuery);
+        $getUserStatement->bindValue('username', $username, PDO::PARAM_STR);
+        $getUserStatement->execute();
+        $selectUser = $getUserStatement->fetch();
+        
+        if(isset($selectUser['Password']))
+        {
+            if($password == $selectUser['Password'])
+            {
+                
+            $_SESSION['username'] = $username;
+            $_SESSION['userId'] = $selectUser['UserId'];
+            $_SESSION['Logged_In'] = true;
+            
+            if($selectUser['Role'] == 0 )
+            {
+            $_SESSION['admin'] = true;
+            }
+
+            header("Location: reviews.php");
+            exit;
+            }
+            elseif($password != $selectUser['Password'])
+            {
+                echo "Incorrect Password";
+            }
+            elseif(!$selectUser)
+            {
+                echo "no user found.";
+            }
+        }
+       
+    }
+}
+
+?>
+
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+<div class="container">
+    <form method="post" action="login.php">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" id="inputField" name="username" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" class="textbox" id="inputField" name="password" placeholder="Password"/>
+            </div>
+            <div>
+                <input type="submit" value="Submit" name="submit" id="btnSubmit" />
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Logged Out</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <h1>You Have logged out!</h1>
+    <h2>come back soon!</h2>
+
+    <h3><a href="reviews.php">Home</a></h3>
+</body>
